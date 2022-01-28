b4554eb3c69728133ee0ff1759f63ec0d929b40f
Samaksh Bansal <samaksh199@gmail.com>
2022-01-28 23:12:07 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Habitify',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Main(),
+    );
+  }
+}
+
+class Main extends StatefulWidget {
+  const Main({Key? key}) : super(key: key);
+
+  @override
+  _MainState createState() => _MainState();
+}
+
+class _MainState extends State<Main> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
