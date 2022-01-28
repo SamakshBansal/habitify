96554bb9f5416fe5ff324acb32a7826c994b7514
Samaksh Bansal <samaksh199@gmail.com>
2022-01-28 23:20:19 +0530

    Bottom Nav Bar

--- lib/main.dart
+++ lib/main.dart
@@ -28,8 +28,68 @@ class Main extends StatefulWidget {
 }
 
 class _MainState extends State<Main> {
+  int currentIndex = 0;
+
+  void change(index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  final screens = [
+    const Center(
+      child: Text(
+        'Journal',
+        style: TextStyle(fontSize: 60),
+      ),
+    ),
+    const Center(
+      child: Text(
+        'Progress',
+        style: TextStyle(fontSize: 60),
+      ),
+    ),
+    const Center(
+      child: Text(
+        'Setting',
+        style: TextStyle(fontSize: 60),
+      ),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: IndexedStack(
+        index: currentIndex,
+        children: screens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.blue,
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.grey.shade400,
+        iconSize: 30,
+        currentIndex: currentIndex,
+        onTap: (index) => change(index),
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.book_outlined),
+            label: 'Journal',
+            // backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.access_time),
+            label: 'Progress',
+            // backgroundColor: Colors.red,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Setting',
+            // backgroundColor: Colors.purple,
+          ),
+        ],
+      ),
+    );
   }
 }
