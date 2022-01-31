84efcc1c4d870909b37a0fe3c674e01e82bdaa84
Samaksh Bansal <samaksh199@gmail.com>
2022-01-31 16:53:22 +0530

    Bottom Nav Bar

--- lib/screens/journal.dart
+++ lib/screens/journal.dart
@@ -9,19 +9,73 @@ class MyJournal extends StatefulWidget {
 }
 
 class _MyJournalState extends State<MyJournal> {
+  TextEditingController _controller =
+      TextEditingController(text: "Festive Leave");
+  bool _isEnable = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController(text: 'My Journal');
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            const SizedBox(
+              height: 10,
+            ),
             Text(
-              '  ' + DateFormat.yMMMEd().format(DateTime.now()),
+              '  ' + DateFormat.MMMd().format(DateTime.now()).toUpperCase(),
               style: const TextStyle(
                 color: Colors.grey,
                 fontSize: 15,
                 fontWeight: FontWeight.bold,
               ),
+            ),
+            Row(
+              children: <Widget>[
+                SizedBox(
+                  width: 170,
+                  child: TextField(
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                    ),
+                    controller: _controller,
+                    enabled: _isEnable,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+                IconButton(
+                    icon: const Icon(Icons.edit),
+                    onPressed: () {
+                      setState(() {
+                        _isEnable = true;
+                      });
+                    })
+              ],
+            ),
+            Row(
+              children: [
+                horizontalList2,
+                IconButton(
+                  icon: const Icon(Icons.add),
+                  onPressed: () {},
+                ),
+              ],
             ),
           ],
         ),
@@ -29,3 +83,62 @@ class _MyJournalState extends State<MyJournal> {
     );
   }
 }
+
+Widget horizontalList2 = Container(
+  margin: const EdgeInsets.symmetric(vertical: 20.0),
+  height: 40,
+  width: 340,
+  child: ListView(
+    scrollDirection: Axis.horizontal,
+    children: <Widget>[
+      ElevatedButton(
+        style: ButtonStyle(
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(18.0),
+        ))),
+        onPressed: () {},
+        child: Row(
+          children: const [
+            Icon(Icons.library_books_outlined),
+            Text('  All Habits'),
+          ],
+        ),
+      ),
+      const SizedBox(
+        width: 20,
+      ),
+      ElevatedButton(
+        style: ButtonStyle(
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(18.0),
+        ))),
+        onPressed: () {},
+        child: Row(
+          children: const [
+            Icon(Icons.folder),
+            Text('  Morning'),
+          ],
+        ),
+      ),
+      const SizedBox(
+        width: 20,
+      ),
+      ElevatedButton(
+        style: ButtonStyle(
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(18.0),
+        ))),
+        onPressed: () {},
+        child: Row(
+          children: const [
+            Icon(Icons.wb_sunny),
+            Text('  Afternoon'),
+          ],
+        ),
+      ),
+    ],
+  ),
+);
